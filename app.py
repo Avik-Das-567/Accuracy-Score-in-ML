608b0d2c4910030b6b6c4320fd58db65e9e4ff1c
Avik Das <avikdas567@gmail.com>
2025-06-02 22:32:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,7 @@ y_true = [1, 0, 1, 1, 0]
 y_pred = [1, 0, 1, 0, 0]
 
 accuracy = accuracy_score(y_true, y_pred)
+
+st.write("Actual Results:", y_true)
+st.write("Predicted Results:", y_pred)
 st.write("Accuracy Score:", accuracy)
